1b92e45dd6b90a1fb16d08d55b8f73cd26e14e8c
joyj10 <micool1030@gmail.com>
2023-08-24 07:41:23 +0900

    # 웹 계층 작업 : home, member commit

--- src/main/java/jpabook/jpashop/controller/HomeController.java
+++ src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -1,8 +1,26 @@
-package jpabook.jpashop.controller;/**
+package jpabook.jpashop.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
  * HomeController
  * <pre>
  * Describe here
  * </pre>
+ *
  * @version 1.0,
- */public class HomeController {
+ */
+
+@Controller
+@Slf4j
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        log.info("### home contoller");
+        return "home";
+    }
 }
--- src/main/java/jpabook/jpashop/controller/MemberForm.java
+++ src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -1,8 +1,25 @@
-package jpabook.jpashop.controller;/**
+package jpabook.jpashop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+/**
  * MemberForm
  * <pre>
  * Describe here
  * </pre>
+ *
  * @version 1.0,
- */public class MemberForm {
+ */
+
+@Getter @Setter
+public class MemberForm {
+    @NotEmpty(message = "회원 이름은 필수 입니다.")
+    private String name;
+
+    private String city;
+    private String street;
+    private String zipcode;
 }
