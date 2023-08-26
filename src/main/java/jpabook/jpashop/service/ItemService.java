be32855a18eb09bff53bfb296a7babe62b64cae5
joyj10 <micool1030@gmail.com>
2023-08-26 15:01:31 +0900

    # 웹 계층 작업 : 상품 수정

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,16 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Book findItem = (Book) itemRepository.findOne(itemId);
+
+        // TODO 리팩토링 : set 보다는 의미있는 메서드를 엔티티에 change 로 추가하여 하는게 맞음(유지보수를 위해)
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public List<Item> findItems() {
