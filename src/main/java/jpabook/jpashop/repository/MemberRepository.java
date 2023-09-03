fbd01fad8ad377c7f45c7c7c0c77ebb485e48222
joyj10 <micool1030@gmail.com>
2023-09-03 12:21:14 +0900

    Revert "spring data jpa & querydsl"
    This reverts commit 9a368ee46d1c6d6250a0055dd8c08a7de8b0e6ba.

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,8 +1,11 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
 
 /**
  * MemberRepository
@@ -14,6 +17,33 @@ import org.springframework.stereotype.Repository;
  */
 
 @Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    Long countByName(String name);
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+
+    public Long countByName(String name) {
+        return em.createQuery("select count(m) from Member m where m.name = :name", Long.class)
+                .setParameter("name", name)
+                .getSingleResult();
+    }
 }
